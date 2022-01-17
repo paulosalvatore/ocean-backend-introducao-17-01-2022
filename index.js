81de97ceaa208b848df86ecc2236e26d5ec98119
Paulo Salvatore <salvatoregames@gmail.com>
2022-01-17 12:03:17 -0300

    Implementação do Read All

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ app.get("/", function (req, res) {
     res.send("Hello World");
 });
 
+const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+
+// [GET] "/herois" - Read All (Ler todos os registros)
+app.get("/herois", function (req, res) {
+    res.send(lista);
+});
+
 app.listen(3000);
