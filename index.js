284d77f468655f0ea64ab79ccbefff8c2a86e99e
Paulo Salvatore <salvatoregames@gmail.com>
2022-01-17 13:28:30 -0300

    Criação do endpoint `/oi`

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 
+// Endpoint "/"
 app.get("/", function (req, res) {
     res.send("Hello World");
 });
 
+// Endpoint "/oi"
+app.get("/oi", function (req, res) {
+    res.send("Olá, mundo!");
+});
+
 const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+//              0                   1                2
 
 // [GET] "/herois" - Read All (Ler todos os registros)
 app.get("/herois", function (req, res) {
